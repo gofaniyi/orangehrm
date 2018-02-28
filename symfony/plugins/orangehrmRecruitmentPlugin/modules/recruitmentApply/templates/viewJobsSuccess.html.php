fe7b01dd531723e7c91f72cff95358a207fae399
user <user@users-MacBook-Air.local>
2018-03-01 00:41:21 +0100

    Working on redesigning the viewJobSuccessPage

--- symfony/plugins/orangehrmRecruitmentPlugin/modules/recruitmentApply/templates/viewJobsSuccess.html.php
+++ symfony/plugins/orangehrmRecruitmentPlugin/modules/recruitmentApply/templates/viewJobsSuccess.html.php
@@ -1,137 +1,270 @@
-<?php use_javascript(plugin_web_path('orangehrmRecruitmentPlugin', 'js/viewJobsSuccess')); ?>
 
-<style type="text/css">
+
     
-    #content {
-        padding-top: 0;
-    }    
-    
-    pre {
-        overflow: auto;
-        white-space: pre-wrap;       /* css-3 */
-        white-space: -moz-pre-wrap !important;  /* Mozilla, since 1999 */
-        white-space: -pre-wrap;      /* Opera 4-6 */
-        white-space: -o-pre-wrap;    /* Opera 7 */
-        word-wrap: break-word;       /* Internet Explorer 5.5+ */
-    }
-    
-    #toggleJobList {
-        float: right;
-        margin: -2px 10px 0px 0px;
-        font-size: 12px;
-    }
+    <link href="<?php echo theme_path('assets/css/app.min.css')?>" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo theme_path('assets/css/thejobs.css')?>" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo theme_path('assets/css/custom.css')?>" rel="stylesheet" type="text/css"/>
 
-    #toggleJobList span {
-        text-decoration: underline;
-        cursor: pointer; 
-    }
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Oswald:100,300,400,500,600,800%7COpen+Sans:300,400,500,600,700,800%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
 
-    .vacancyDescription, .vacancyShortDescription {
-        display: none;
-        line-height: 15px;
-        margin-bottom: 10px;
-    }
 
-    .vacancyShortDescription {
-        display: block;
-    }
+  <body class="nav-on-header smart-nav">
 
-    .applyLink {
-        display: none;
-    }
+    <!-- Navigation bar -->
+    <nav class="navbar">
+      <div class="container">
 
-    .vacancyTitle :hover {
-        cursor: pointer;
-    }
+        <!-- Logo -->
+        <div class="pull-left">
+          <a class="navbar-toggle" href="#" data-toggle="offcanvas"><i class="ti-menu"></i></a>
 
-    .plusOrMinusmark {
-        text-align: right;
-        margin-top: 10px;
-        padding-right: 3px;
-        font-size: 12px;
-        position: relative;
-        top: 12px;
-        right: 8px;
-    }
-
-    .plusMark, .minusMark {
-        cursor: pointer;
-    }
-
-    .minusMark {
-        display: none;
-    }
-
-    h3 {
-        margin-bottom: 10px;
-    }
-    
-</style>
-
-<div id="jobPage">
-    <div class="box">
-        <div class="maincontent">
-            <div class="head">
-                <h1><?php echo __('Active Job Vacancies'); ?></h1>
-            </div>
-
-            <div class="inner">
-                <?php if (count($publishedVacancies) != 0): ?>                    
-                    <div id="toggleJobList">
-                        <span id="expandJobList"><?php echo __('Expand all') ?></span> | <span id="collapsJobList"><?php echo __('Collapse all'); ?></span>
-                    </div>
-
-                    <?php foreach ($publishedVacancies as $vacancy): ?>
-
-                        <div class="plusOrMinusmark">
-                            <span class="plusMark">[+]</span><span class="minusMark">[-]</span>
-                        </div>
-
-                        <div class="jobItem">
-
-                            <div class="vacancyTitle">
-                                <h3><?php echo $vacancy->getName(); ?></h3>
-                            </div>
-
-                            <pre class="vacancyShortDescription"><?php echo getShortDescription($vacancy->getDescription(), 250, "..."); ?></pre>
-                            <pre class="vacancyDescription"><?php echo $vacancy->getDescription(); ?></pre>
-                            
-                            <p class="borderBottom">
-                                <input type="button" class="apply" name="applyButton" value="<?php echo __("Apply"); ?>" onmouseout="moutButton(this);" onmouseover="moverButton(this);" />
-                                <a href="<?php echo public_path('index.php/recruitmentApply/applyVacancy/id/' . $vacancy->getId(), true); ?>" class="applyLink"></a>
-                            </p>
-                            
-                        </div>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <span class="noVacanciesMessage"><?php echo __('No active job vacancies to display'); ?></span>
-                <?php endif; ?>
-
-            </div>
+          <div class="logo-wrapper">
+            <a class="logo" href="index.html"><img src="<?php echo theme_path('assets/img/logo.png')?>" alt="logo"></a>
+            <a class="logo-alt" href="index.html"><img src="<?php echo theme_path('assets/img/logo-alt.png')?>" alt="logo-alt"></a>
+          </div>
 
         </div>
+        <!-- END Logo -->
 
-    </div>
+      </div>
+    </nav>
+    <!-- END Navigation bar -->
 
-</div>
-<div id="footer">
-    <?php include_partial('global/copyright');?>
-</div>
-<?php
-/*
- * Get short description to show in default view in view job list
- * @param string $description full description
- * @param int $limit Number of characters show in short description
- * @param string $endString String added to end of the short description
- * @return string $description short description 
- */
 
-function getShortDescription($description, $limit, $endString) {
+    <!-- Site header -->
+    <header class="site-header size-lg text-center" style="background-image: url(<?php echo theme_path('assets/img/bg-banner1.jpg')?>)">
+      <div class="container">
+        <div class="col-xs-12">
+          <br><br>
+          <h2><?php echo __('We offer'); ?> <mark><?php echo __('1,259'); ?></mark> <?php echo __('job vacancies right now!'); ?></h2>
+          <h5 class="font-alt"><?php echo __('Find your desire one in a minute '); ?></h5>
+          <br><br><br>
+        </div>
 
-    if (strlen($description) > $limit) {
-        $subString = substr($description, 0, $limit);
-        $wordArray = explode(" ", $subString);
-        $description = substr($subString, 0, -(strlen($wordArray[count($wordArray) - 1]) + 1)) . $endString;
-    }
-    return $description;
-}
+      </div>
+    </header>
+    <!-- END Site header -->
+
+
+    <!-- Main container -->
+    <main>
+
+
+
+      <!-- Recent jobs -->
+      <section>
+        <div class="container">
+          <header class="section-header">
+            <span><?php echo __('Latest'); ?></span>
+            <h2><?php echo __('Recent Jobs'); ?></h2>
+          </header>
+
+          <div class="row item-blocks-connected">
+
+            <?php if (count($publishedVacancies) != 0): ?>   
+
+                <?php foreach ($publishedVacancies as $vacancy): ?>
+
+                    <!-- Job item -->
+                    <div class="col-xs-12">
+                      <a class="item-block" href="job-detail.html">
+                        <header>
+                          <div class="hgroup">
+                            <h4><?php echo $vacancy->getName(); ?></h4>
+                            <h5>Google</h5>
+                          </div>
+                          <div class="header-meta">
+                            <span class="location">Menlo park, CA</span>
+                            <span class="label label-success">Full-time</span>
+                          </div>
+                        </header>
+                      </a>
+                    </div>
+                    <!-- END Job item -->
+                <?php endforeach; ?>
+
+
+          </div>
+
+          <br><br>
+          <p class="text-center"><a class="btn btn-info" href="job-list.html">Browse all jobs</a></p>
+
+          <?php else: ?>
+                <span class="noVacanciesMessage"><?php echo __('No active job vacancies to display'); ?></span>
+            <?php endif; ?>
+        </div>
+      </section>
+      <!-- END Recent jobs -->
+
+
+      <!-- Facts -->
+      <section class="bg-img bg-repeat no-overlay section-sm" style="background-image: url(<?php echo theme_path('assets/img/bg-pattern.png')?>)">
+        <div class="container">
+
+          <div class="row">
+            <div class="counter col-md-3 col-sm-6">
+              <p><span data-from="0" data-to="6890"></span>+</p>
+              <h6>Jobs</h6>
+            </div>
+
+            <div class="counter col-md-3 col-sm-6">
+              <p><span data-from="0" data-to="1200"></span>+</p>
+              <h6>Members</h6>
+            </div>
+
+            <div class="counter col-md-3 col-sm-6">
+              <p><span data-from="0" data-to="36800"></span>+</p>
+              <h6>Resume</h6>
+            </div>
+
+            <div class="counter col-md-3 col-sm-6">
+              <p><span data-from="0" data-to="15400"></span>+</p>
+              <h6>Company</h6>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <!-- END Facts -->
+
+
+      <!-- Categories -->
+      <section class="bg-alt">
+        <div class="container">
+          <header class="section-header">
+            <span>Categories</span>
+            <h2>Popular categories</h2>
+            <p>Here's the most popular categories</p>
+          </header>
+
+          <div class="category-grid">
+            <a href="job-list-1.html">
+              <i class="fa fa-laptop"></i>
+              <h6>Technology</h6>
+              <p>Designer, Developer, IT Service, Front-end developer, Project management</p>
+            </a>
+
+            <a href="job-list-2.html">
+              <i class="fa fa-line-chart"></i>
+              <h6>Accounting</h6>
+              <p>Finance, Tax service, Payroll manager, Book keeper, Human resource</p>
+            </a>
+
+            <a href="job-list-3.html">
+              <i class="fa fa-medkit"></i>
+              <h6>Medical</h6>
+              <p>Doctor, Nurse, Hospotal, Dental service, Massagist</p>
+            </a>
+
+            <a href="job-list-1.html">
+              <i class="fa fa-cutlery"></i>
+              <h6>Food</h6>
+              <p>Restaurant, Food service, Coffe shop, Cashier, Waitress</p>
+            </a>
+
+            <a href="job-list-2.html">
+              <i class="fa fa-newspaper-o"></i>
+              <h6>Media</h6>
+              <p>Journalism, Newspaper, Reporter, Writer, Cameraman</p>
+            </a>
+
+            <a href="job-list-3.html">
+              <i class="fa fa-institution"></i>
+              <h6>Government</h6>
+              <p>Federal, Law, Human resource, Manager, Biologist</p>
+            </a> 
+          </div>
+
+        </div>
+      </section>
+      <!-- END Categories -->
+
+
+      <!-- Newsletter -->
+      <section class="bg-img text-center" style="background-image: url(assets/img/bg-facts.jpg)">
+        <div class="container">
+          <h2><strong>Subscribe</strong></h2>
+          <h6 class="font-alt">Get weekly top new jobs delivered to your inbox</h6>
+          <br><br>
+          <form class="form-subscribe" action="#">
+            <div class="input-group">
+              <input type="text" class="form-control input-lg" placeholder="Your eamil address">
+              <span class="input-group-btn">
+                <button class="btn btn-success btn-lg" type="submit">Subscribe</button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </section>
+      <!-- END Newsletter -->
+
+
+    </main>
+    <!-- END Main container -->
+
+
+    <!-- Site footer -->
+    <footer class="site-footer">
+
+      <!-- Top section -->
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">An employment website is a web site that deals specifically with employment or careers. Many employment websites are designed to allow employers to post job requirements for a position to be filled and are commonly known as job boards. Other employment sites offer employer reviews, career and job-search advice, and describe different job descriptions or employers. Through a job website a prospective employee can locate and fill out a job application.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Company</h6>
+            <ul class="footer-links">
+              <li><a href="page-about.html">About us</a></li>
+              <li><a href="page-typography.html">How it works</a></li>
+              <li><a href="page-faq.html">Help center</a></li>
+              <li><a href="page-typography.html">Privacy policy</a></li>
+              <li><a href="page-contact.html">Contact us</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Trendeing jobs</h6>
+            <ul class="footer-links">
+              <li><a href="job-list.html">Front-end developer</a></li>
+              <li><a href="job-list.html">Android developer</a></li>
+              <li><a href="job-list.html">iOS developer</a></li>
+              <li><a href="job-list.html">Full stack developer</a></li>
+              <li><a href="job-list.html">Project administrator</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <hr>
+      </div>
+      <!-- END Top section -->
+
+      <!-- Bottom section -->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyrights &copy; 2016 All Rights Reserved by <a href="http://themeforest.net/user/shamsoft">ShaMSofT</a>.</p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- END Bottom section -->
+
+    </footer>
+    <!-- END Site footer -->
+
+
+  </body>
+</html>
